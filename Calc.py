87eebfb49ec7f4eff082b285036e04e7e9085b78
김동윤 <pctiger01@kyonggi.ac.kr>
2024-10-04 09:05:20 +0900

    파이썬 계산기

---
+++ Calc.py
@@ -0,0 +1,11 @@
+# 두 정수를 입력받기
+num1 = int(input("첫 번째 정수를 입력하세요: "))
+num2 = int(input("두 번째 정수를 입력하세요: "))
+
+# 합과 차를 계산
+sum = num1 + num2
+diff = num1 - num2
+
+# 결과 출력
+print(f"두 수의 합: {sum}")
+print(f"두 수의 차: {diff}")
